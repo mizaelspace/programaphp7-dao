18d58ab1b8fc012e3994a6a4e4cb236a7677a0b7
Mizaellf <mizaelnsjc@gmail.com>
2019-09-15 10:47:26 -0300

    Iniciamos o projeto

---
+++ class/Sql.php
@@ -0,0 +1,50 @@
+<?php
+
+class Sql extends PDO
+{
+	private $conn; //Trazendo a conexão
+
+	public function __construct() //Quando fazer a consulta vai entrar automaticamento no banco de dados
+	{
+
+		$this->conn = new PDO("mysql:host=127.0.0.1;dbname=dbphp7","root","zenildo.9"); //Conectando no banco
+	}
+
+	private function setParams($statment, $parameters = array()) //Chamando os parametros da setParam
+	{
+		foreach ($parameters as $key => $value) 
+		{
+			$this->setParam($key, $value);
+		}
+	}
+
+	private function setParam($statment, $key, $value) //Trazendo o parametro
+	{
+
+		$statment->bindParam($key, $value);
+	}
+
+	public function query($rawQuery, $params = array())
+	{
+		$stmt = $this->conn->prepare($rawQuery); //Prepare vem da classe pai
+
+		$this->setParams($stmt, $params);
+		
+		$stmt->execute();
+
+		return $stmt;
+	}
+
+
+	public function select($rawQuery, $params = array()):array
+	{
+		$stmt = $this->query($rawQuery, $params);
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
+	
+}
+
+
+
+
+?>
